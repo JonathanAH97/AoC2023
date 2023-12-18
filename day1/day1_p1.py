499d8baa0238694e963006128f3480df850cb6c0
JonathanAH97 <80654383+JonathanAH97@users.noreply.github.com>
2023-12-18 19:55:18 +0100

    Day1

---
+++ day1/day1_p1.py
@@ -0,0 +1,24 @@
+with open("input.txt") as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+
+# Find calibration value for a single line
+# Search for the first number by going forwards in the string and stopping when a number is found
+# Search for the second number by going backwards in the string and stopping when a number is found
+def find_cal(line):
+
+    for s in line:
+        if s.isdigit():
+            first_num = s
+            break
+
+    for s in line[::-1]:
+        if s.isdigit():
+            second_num = s
+            break
+
+    return  int(first_num + second_num)
+
+cal_list = [find_cal(line) for line in lines]
+print(sum(cal_list))
